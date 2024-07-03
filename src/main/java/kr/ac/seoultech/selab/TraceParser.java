eeedef2561ec2aa4c3014ed2d1d080689e5f8ddc
Hyogeun Park <hyotaime@hyotaime.com>
2024-07-03 19:41:12 +0900

    Revert "7/3 메서드 모두 출력"

--- src/main/java/kr/ac/seoultech/selab/TraceParser.java
+++ src/main/java/kr/ac/seoultech/selab/TraceParser.java
@@ -7,16 +7,13 @@ import com.google.gson.JsonParser;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class TraceParser {
 
-    public static List<TestDTO> parseJson(String jsonFilePath) {
-        //TestDTO 객체 만들고
-        List<TestDTO> target = new ArrayList<>();
+    public static Map<String, Map<String, Integer>> parseJson(String jsonFilePath) {
+        Map<String, Map<String, Integer>> targetMethods = new HashMap<>();
 
         try (Reader reader = new InputStreamReader(
                 TraceParser.class.getClassLoader().getResourceAsStream(jsonFilePath))) {
@@ -25,19 +22,11 @@ public class TraceParser {
             }
             JsonElement jsonElement = JsonParser.parseReader(reader);
             JsonObject jsonObject = jsonElement.getAsJsonObject();
-            JsonArray tracesArray = jsonObject.getAsJsonArray("npe.traces"); // [ { test.class test.method traces [] } , { test.class test.method traces [] } ... ]
+            JsonArray tracesArray = jsonObject.getAsJsonArray("npe.traces");
 
             for (JsonElement traceElement : tracesArray) {
                 JsonObject traceObject = traceElement.getAsJsonObject();
-
-                //test.class 와 test.method test.source로 TestDTO 만들고 put 해줌.
-                String testClassName = traceObject.get("test.class").getAsString();
-                String testMethodName = traceObject.get("test.method").getAsString();
-                TestDTO testDTO = new TestDTO(testClassName, testMethodName);
-                target.add(testDTO);
-
-                JsonArray traceDetails = traceObject.getAsJsonArray("traces");             // [ {class method line is_target}. {class method line is_target} , ... ]
-
+                JsonArray traceDetails = traceObject.getAsJsonArray("traces");
 
                 for (JsonElement detailElement : traceDetails) {
                     JsonObject detailObject = detailElement.getAsJsonObject();
@@ -46,23 +35,8 @@ public class TraceParser {
                         String className = detailObject.get("class").getAsString();
                         String methodName = detailObject.get("method").getAsString();
                         int lineNumber = detailObject.get("line").getAsInt();
-                        //className에 "Test"있는지 확인하고
-                        if(JDTMethodExtractor.isContainTest(className)){
-                            //"Test" 있으면 기존 TestDTO에 line 필드 추가 (npe.trace.json에 각각 객체에는 Test 클래스가 하나씩밖에 없음)
-                            testDTO.setTestLine(lineNumber);
-                        }
-                        else {
-                            //"Test" 없으면 TestDTO의 source에 기존 class와 method 동시에 겹치는게 있는지 확인하고
-                            if(testDTO.isDuplicate(className, methodName)){
-                                //있다면 SourceDTO 안의 sourceLine 리스트에 해당 라인값만 추가
-                                SourceDTO sourceDTO = testDTO.findSourceDTO(className, methodName);
-                                sourceDTO.getSourceLine().get(lineNumber);
-                            }
-                            else{
-                                //없다면 source 필드에 list.add(new SourceDTO(class,method,line))
-                                testDTO.getSource().add(new SourceDTO(className, methodName, lineNumber));
-                            }
-                        }
+
+                        targetMethods.computeIfAbsent(className, k -> new HashMap<>()).put(methodName, lineNumber);
                     }
                 }
             }
@@ -70,6 +44,6 @@ public class TraceParser {
             e.printStackTrace();
         }
 
-        return target;
+        return targetMethods;
     }
 }
